13ce48d6185144e7ac2d1a680c485dd3476be7fd
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-12-07 23:52:48 +0800

    Refactor code

--- tests/youtube.test.js
+++ tests/youtube.test.js
@@ -1,21 +1,38 @@
-const youtube = require('../lib/youtube');
+const {when, resetAllWhenMocks} = require("jest-when");
+const youtube = require("../lib/youtube");
 
-jest.mock('axios');
-const axios = require('axios');
+require("../lib/constants");
+jest.mock("../lib/constants", () => ({
+  youtubeChannelId: "some-channel-id",
+  youtubeApiKey: "SOME-API-KEY"
+}));
 
-describe('YouTube', () => {
-    beforeEach(() => {
-        const response = {
-            data: {
-                items: [{ id: { videoId: 'ABC123' } }]
-            }
-        };
+jest.mock("axios");
+const axios = require("axios");
 
-        axios.get.mockResolvedValue(response);
-    });
+const response = {
+  data: {
+    items: [{id: {videoId: "ABC123"}}]
+  }
+};
 
-    it('returns a valid YouTube video URL', async () => {
-        const video = await youtube.video();
-        expect(video).toBe('http://www.youtube.com/watch?v=ABC123');
-    });
+const url =
+  "https://www.googleapis.com/youtube/v3/search?channelId=some-channel-id&key=SOME-API-KEY&maxResults=50&part=snippet";
+
+describe("YouTube", () => {
+  afterEach(() => {
+    resetAllWhenMocks();
+  });
+
+  it("returns a valid YouTube video URL", async () => {
+    when(axios.get)
+      .expectCalledWith(url)
+      .mockResolvedValue(response);
+
+    const channelId = "some-channel-id";
+    const apiKey = "SOME-API-KEY";
+    const video = await youtube.video(channelId, apiKey);
+
+    expect(video).toBe("http://www.youtube.com/watch?v=ABC123");
+  });
 });
